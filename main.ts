65e4c6f8f910c365f9698c012be7149af121641b
Devon-Grder <132303368+Devon-Grder@users.noreply.github.com>
2024-10-05 23:22:59 -0400

    Finished colisions

--- main.ts
+++ main.ts
@@ -8,6 +8,17 @@ namespace SpriteKind {
 /**
  * Look at SpawnRowOfBlocks function in Araknoid clone
  */
+sprites.onOverlap(SpriteKind.YellowBlock, SpriteKind.Food, function (sprite, otherSprite) {
+    bounce(otherSprite)
+    otherSprite.setPosition(otherSprite.x, sprite.top + 3)
+    mySprite2 = sprites.create(img`
+        2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 
+        `, SpriteKind.RedBlock)
+    mySprite2.setPosition(sprite.x, sprite.y)
+})
 function createBoundBox () {
     mySprite = sprites.create(img`
         ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
@@ -87,16 +98,6 @@ function SpawnBall () {
     Ball.setVelocity(ballXSpeed, BallYSPeed)
     Ball.setBounceOnWall(true)
 }
-sprites.onOverlap(SpriteKind.YellowBlock, SpriteKind.Food, function (sprite, otherSprite) {
-    bounce(otherSprite)
-    otherSprite.setPosition(otherSprite.x, sprite.top + 3)
-    mySprite2 = sprites.create(img`
-        2 2 2 2 2 2 2 2 
-        2 2 2 2 2 2 2 2 
-        2 2 2 2 2 2 2 2 
-        2 2 2 2 2 2 2 2 
-        `, SpriteKind.RedBlock)
-})
 function SpawnPaddle () {
     paddle = sprites.create(img`
         9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
@@ -132,6 +133,7 @@ sprites.onOverlap(SpriteKind.BlueBlock, SpriteKind.Food, function (sprite, other
         5 5 5 5 5 5 5 5 
         5 5 5 5 5 5 5 5 
         `, SpriteKind.YellowBlock)
+    mySprite2.setPosition(sprite.x, sprite.y)
 })
 sprites.onOverlap(SpriteKind.Food, SpriteKind.BoundingBox, function (sprite, otherSprite) {
     sprites.destroy(paddle)
@@ -140,16 +142,6 @@ sprites.onOverlap(SpriteKind.Food, SpriteKind.BoundingBox, function (sprite, oth
     SpawnPaddle()
     pause(500)
     SpawnBall()
-})
-sprites.onOverlap(SpriteKind.GreenBlock, SpriteKind.Food, function (sprite, otherSprite) {
-    bounce(otherSprite)
-    otherSprite.setPosition(otherSprite.x, sprite.top + 3)
-    mySprite2 = sprites.create(img`
-        8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 
-        8 8 8 8 8 8 8 8 
-        `, SpriteKind.BlueBlock)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     bounce(otherSprite)
@@ -161,13 +153,23 @@ sprites.onOverlap(SpriteKind.RedBlock, SpriteKind.Food, function (sprite, otherS
     sprites.destroy(sprite)
     BlocksOnScreen += -1
 })
+sprites.onOverlap(SpriteKind.GreenBlock, SpriteKind.Food, function (sprite, otherSprite) {
+    bounce(otherSprite)
+    otherSprite.setPosition(otherSprite.x, sprite.top + 3)
+    mySprite2 = sprites.create(img`
+        8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 
+        8 8 8 8 8 8 8 8 
+        `, SpriteKind.BlueBlock)
+    mySprite2.setPosition(sprite.x, sprite.y)
+})
 let PickBlockVariable = 0
 let paddleSpeed = 0
 let paddle: Sprite = null
 let BallYSPeed = 0
 let ballXSpeed = 0
 let Ball: Sprite = null
-let mySprite2: Sprite = null
 let CurrentBlock: Sprite = null
 let gap = 0
 let BlockY = 0
@@ -181,6 +183,7 @@ let BlueBlock: Sprite = null
 let YellowBlock: Sprite = null
 let RedBlock: Sprite = null
 let mySprite: Sprite = null
+let mySprite2: Sprite = null
 info.setLife(50)
 SpawnBlocks(1)
 SpawnBall()
